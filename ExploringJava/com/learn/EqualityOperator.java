495846f6de58a7bde9f1cdd93be4d383a9e30c11
shruthi <shruthi@thirumals-air>
2023-02-16 20:58:44 -0500

    feb16_commit2

---
+++ ExploringJava/com/learn/EqualityOperator.java
@@ -0,0 +1,18 @@
+package com.learn;
+
+public class EqualityOperator {
+
+	public static void main(String[] args) {
+		int x=10,y=12;
+		if(x<=y)
+		{
+			System.out.println("x is bigger");
+			
+		}
+		else 
+		{
+			System.out.println("y is bigger");
+		}
+	}
+
+}
