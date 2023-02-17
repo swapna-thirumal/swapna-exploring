029b772a61c4f9a9657a671ee2ef7b41631deca1
shruthi <shruthi@thirumals-air>
2023-02-17 08:58:26 -0500

    feb17-commit

---
+++ ExploringJava/com/learn/SimpleArray.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+public class SimpleArray {
+
+	public static void main(String[] args) {
+		int[] x = new int[3] ;
+		System.out.println(x);
+		System.out.println(x[0]);
+		
+	}
+
+}
