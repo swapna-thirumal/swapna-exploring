9e0e0257baf762e63dec7a35abb8d3b112e99ca9
shruthi <shruthi@thirumals-air>
2023-02-16 20:17:57 -0500

    added additon java program

---
+++ ExploringJava/com/learn/Addition.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		int c = a + b ;
+		System.out.println("Added value is "+c);
+
+	}
+
+}
