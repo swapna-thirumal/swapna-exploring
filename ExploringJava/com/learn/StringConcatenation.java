ac46529f2de84da438c59620fee03767f6ff91ef
shruthi <shruthi@thirumals-air>
2023-02-16 20:52:27 -0500

    feb16-commit1

--- ExploringJava/com/learn/StringConcatenation.java
+++ ExploringJava/com/learn/StringConcatenation.java
@@ -5,7 +5,7 @@ public class StringConcatenation {
 	public static void main(String[] args) {
 		int a=33;
 		String s="swapna";
-		System.out.println(s+a);
+		System.out.println(s+ " " +a);
 				
 	}
 
