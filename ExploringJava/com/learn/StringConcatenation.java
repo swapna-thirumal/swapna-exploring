18447e52f931d10f3726e761a7049630c229164d
shruthi <shruthi@thirumals-air>
2023-02-16 20:49:24 -0500

    feb16commit1

---
+++ ExploringJava/com/learn/StringConcatenation.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		int a=33;
+		String s="swapna";
+		System.out.println(s+a);
+				
+	}
+
+}
+;
