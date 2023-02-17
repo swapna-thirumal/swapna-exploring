7dbaa0dc39bd3b1fddcac694c89e37128d050b16
shruthi <shruthi@thirumals-air>
2023-02-17 10:53:40 -0500

    feb17--commit

---
+++ ExploringJava/com/learn/SimpleArrayLength.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+public class SimpleArrayLength {
+
+	public static void main(String[] args) {
+		int[] x = new int[6];
+		int[] y = {1,2,3,4,5};
+		System.out.println(x.length);
+		System.out.println(y.length);
+		
+	}
+
+}
