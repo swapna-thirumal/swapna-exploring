91c74f969dd4023ab15cd131fadf2bc40a200e43
shruthi <shruthi@thirumals-air>
2023-02-17 10:32:16 -0500

    feb17-c0mmit

---
+++ ExploringJava/com/learn/SimpleArray2.java
@@ -0,0 +1,14 @@
+package com.learn;
+
+public class SimpleArray2 {
+
+	public static void main(String[] args) {
+		int[] x = {10,20,30};
+		int[] y= new int[2];
+		
+		System.out.println(x[2]+ " " +x[0]);
+		
+		System.out.println(y[0]);
+	}
+
+}
