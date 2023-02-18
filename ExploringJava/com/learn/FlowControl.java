428ebc64c6debac2ab9410f432e759110016f48f
shruthi <shruthi@thirumals-air>
2023-02-18 16:02:54 -0500

    FEB18commit

--- ExploringJava/com/learn/FlowControl.java
+++ ExploringJava/com/learn/FlowControl.java
@@ -4,12 +4,17 @@ public class FlowControl {
 
 	public static void main(String[] args) {
 		int i,j;
-		for (i=1;i<=6;i++)
+		
+		
+		for (i=1;i<=4;i++)
 		{
-		 for(j=1;j<=i;j++)
-		 {
-			System.out.print(j+" ");
+			char c=65;
 			
+		 for(j=1;j<=i;j++)
+			 
+		 {
+			System.out.print(c +" ");
+			c++;
 		 }
 			System.out.println();
 		}
