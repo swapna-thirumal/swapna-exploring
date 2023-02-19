9de8415c47e273b84ade09a971999f53b68d4f88
shruthi <shruthi@thirumals-air>
2023-02-19 12:47:04 -0500

    Feb19-commit

---
+++ ExploringJava/com/learn/SimpleArray11.java
@@ -0,0 +1,18 @@
+package com.learn;
+
+public class SimpleArray11 {
+
+	public static void main(String[] args) {
+		int i=22;
+		int[] x= {10,20,30,40};
+		
+		if(x[0]<=1)
+		{
+			System.out.print("i is bigger");
+		}
+		else
+			System.out.println(++i);
+
+	}
+
+}
