165cc87b1c0250535b878199469015c8edc5680e
shruthi <shruthi@thirumals-air>
2023-02-21 15:58:52 -0500

    feb21-commit

---
+++ ExploringJava/com/learn1/Test.java
@@ -0,0 +1,14 @@
+package com.learn1;
+
+class Test{
+
+public static void main(String[] args)
+{
+	String[] argh= {"10","20","30"};
+	
+	args=argh;
+	
+	System.out.println(args[0]+args[1]+args[2]);
+}
+
+}
