b53b589e8ed17c2b0201fc0776a516850ea0b546
shruthi <shruthi@thirumals-air>
2023-02-22 17:35:14 -0500

    feb22-commiT

---
+++ ExploringJava/com/learn/SimpleArray4.java
@@ -0,0 +1,21 @@
+package com.learn;
+
+public class SimpleArray4
+{
+
+	public static void main(String[] args)
+	{ 
+		int[] x = new int[5];
+	       x[0]=1;
+	       x[1]=11;
+	       x[2]=111;
+	       for(int i=0;i<=4;i++) 
+	       {
+	       
+	       System.out.print(" " + x[i]);
+	       
+	       }
+		
+	}
+
+}
