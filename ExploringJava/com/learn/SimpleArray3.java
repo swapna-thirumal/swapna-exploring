4b8f7dc804900a2746e64828f507c739b877f2a4
shruthi <shruthi@thirumals-air>
2023-02-22 15:46:28 -0500

    feb22-commit

---
+++ ExploringJava/com/learn/SimpleArray3.java
@@ -0,0 +1,22 @@
+package com.learn;
+
+public class SimpleArray3 {
+
+	public static void main(String[] args)
+	{
+		int[][] x = {{10,20,30},{20,40,50}};
+		
+		for(int i=0;i<2;i++)
+	  {
+	    for(int j=0;j<3;j++)
+	     {
+	    	System.out.print(" " + x[i][j]);
+	     }
+	    System.out.println();
+	        
+	  }
+			
+		
+	} 
+
+}
