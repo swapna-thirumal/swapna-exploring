3cf58516487b9becfb41b4de0c496d53c4a124bb
shruthi <shruthi@thirumals-air>
2023-02-22 11:50:43 -0500

    feb22-commit

---
+++ ExploringJava/com/learn/FlowControl4.java
@@ -0,0 +1,26 @@
+package com.learn;
+
+public class FlowControl4 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i=5;i>=0;i--)
+		    {
+			
+			for(int j=i; j>=0;j--)
+				
+			  {
+				
+				
+				System.out.print("  " + j);
+				
+			   }
+			     System.out.println(" ");
+		     
+		    }
+		
+		
+	}
+
+}
