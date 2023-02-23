be6854ebae94bbf215ab6d7487dacc3b9acdcbd6
shruthi <shruthi@thirumals-air>
2023-02-22 21:19:16 -0500

    feb22-commit

---
+++ ExploringJava/com/learn1/ForEach.java
@@ -0,0 +1,16 @@
+package com.learn1;
+
+public class ForEach
+{
+
+	public static void main(String[] args) 
+	{
+		int[] x = {5,6,7,8};
+		for(int x1 : x)
+		{
+			System.out.println("Thr value of x1 is : " + x1);
+		}
+			
+	}
+
+}
