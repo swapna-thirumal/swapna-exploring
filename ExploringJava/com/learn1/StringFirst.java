82a9ec41c1558ba0112b978bec304e7808fe8a9d
shruthi <shruthi@thirumals-air>
2023-02-23 18:36:44 -0500

    feb23-commit

---
+++ ExploringJava/com/learn1/StringFirst.java
@@ -0,0 +1,19 @@
+package com.learn1;
+
+public class StringFirst 
+{
+
+	public static void main(String[] args)
+	{
+		String s = new String("swapna");
+		String s1 =new String("swapna");
+		String a ="shruthi";
+		String b = "shruthi";
+		System.out.println(a==b);
+		System.out.println(s==s1);
+		System.out.println(a.equals(b));
+		System.out.println(s.equals(s1));
+		
+	}
+
+}
