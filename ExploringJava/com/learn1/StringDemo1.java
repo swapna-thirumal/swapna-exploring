d4e7b2bb2a6e99bcbd14080adc4a9924f65c252a
shruthi <shruthi@thirumals-air>
2023-02-23 20:37:29 -0500

    feb23-comMIT

---
+++ ExploringJava/com/learn1/StringDemo1.java
@@ -0,0 +1,14 @@
+package com.learn1;
+
+public class StringDemo1
+{
+	public static void main(String[] args)
+	{
+		String name = "shru";
+		name= name + "thi";
+		System.out.println(name);
+		
+			
+	}
+
+}
