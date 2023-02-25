acf7db1fba87d67aa59883ea6dc82c6e242e8f2b
shruthi <shruthi@thirumals-air>
2023-02-25 16:59:03 -0500

    feb25-commiT

---
+++ ExploringJava/com/learn1/Condtructor1.java
@@ -0,0 +1,22 @@
+package com.learn1;
+class Demo {	
+	  Demo(){
+		System.out.println("constructor excecuted")	;
+	}
+}
+
+public class Condtructor1 
+ {
+
+	public static void main(String[] args)
+	{
+		Demo t = new Demo();
+		Demo t1 = new Demo();
+		Demo t2 = new Demo();
+		Demo t3 = new Demo();
+		
+	}
+ }
+
+
+
