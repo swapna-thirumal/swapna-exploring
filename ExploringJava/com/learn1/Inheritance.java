82d691178645f54dedc4cf463efc7208354c4f66
shruthi <shruthi@thirumals-air>
2023-02-28 18:04:35 -0500

    feb28-commit

--- ExploringJava/com/learn1/Inheritance.java
+++ ExploringJava/com/learn1/Inheritance.java
@@ -10,30 +10,7 @@ package com.learn1;
 		return a-b;
 	}
 }
- class Bren extends Root
-{
-	
-	public int multi(int a, int b)
-	{
-		return a*b;
-	}
-	
-	public int div(int a, int b)
-	{
-		return a/b;
-	}
-}
-class Brench extends Bren
-{
-	
-	public int sqr(int a)
-	{
-		return a*a;
-	}
-	
-}
-
-
+ 
 public class Inheritance 
 {
 
