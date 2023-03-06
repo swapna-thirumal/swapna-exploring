0524fa5a8be64ba7081dbb7ff75dfdb9b259c7c0
shruthi <shruthi@thirumals-air>
2023-03-06 10:00:20 -0500

    mar6-import

---
+++ ExploringJava/com/learn1/Package11.java
@@ -0,0 +1,13 @@
+package com.learn1;
+
+public class Package11
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(Math.sqrt(16));
+		System.out.println(Math.max(16,17));
+
+	}
+
+}
