60c28f651da18c7e09ebfdda1081ce4581836b53
shruthi <shruthi@thirumals-air>
2023-03-10 14:24:32 -0500

    mar10-commit

---
+++ ExploringJava/com/learn1/NewBalance.java
@@ -0,0 +1,15 @@
+package com.learn1;
+import com.learn2.Balance;
+
+public class NewBalance 
+{
+
+	public static void main(String[] args) 
+	{
+		Balance obj = new Balance ("thiru",22.15);
+		Balance obj1 = new Balance("mal",-88.18);
+		obj.show();
+		obj1.show();
+	}
+
+}
