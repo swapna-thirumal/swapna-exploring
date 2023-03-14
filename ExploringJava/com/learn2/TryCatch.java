7a98ed5a99386ab77e7dde4e9358d69ef1d6b846
shruthi <shruthi@thirumals-air>
2023-03-14 12:43:15 -0400

    mar14-commit

---
+++ ExploringJava/com/learn2/TryCatch.java
@@ -0,0 +1,22 @@
+package com.learn2;
+
+public class TryCatch
+{
+public static void main(String[] args) 
+	{
+		int d,a;
+		try
+		{
+			d=0;
+			a=42/d;
+			System.out.println("hi");
+		}
+		catch(ArithmeticException obj)
+		{
+			System.out.println("Division by zero");
+			System.out.println("after catch");
+		}
+
+	}
+
+}
