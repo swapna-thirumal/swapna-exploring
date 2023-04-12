a53ab09621635d65b25601c0cb2652001067f7bf
shruthi <shruthi@thirumals-air>
2023-04-12 09:15:12 -0400

    april12-commit

---
+++ ExploringJava/com/learn4/CollectionDemo26.java
@@ -0,0 +1,23 @@
+package com.learn4;
+import java.util.*;
+
+public class CollectionDemo26
+{
+
+	public static void main(String[] args)
+	{
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		System.out.println(q.peek());
+		//System.out.println(q.element());
+		for(int i=0;i<=10;i++)
+		{
+			q.offer(i);
+		}
+		System.out.println(q);
+		System.out.println(q.peek());
+		System.out.println(q.poll());
+		System.out.println(q);
+		System.out.println(q.element());
+	}
+
+}
---
+++ ExploringJava/com/learn4/CollectionDemo27.java
@@ -0,0 +1,26 @@
+package com.learn4;
+import java.util.*;
+
+public class CollectionDemo27
+{
+
+	public static void main(String[] args) 
+	{
+		PriorityQueue q = new PriorityQueue(12, new QueueComparator());
+		q.offer("sun");
+		q.offer("cloud");
+		q.offer("moon");
+		q.offer("air");
+		q.offer("sky");
+		System.out.println(q);
+	}
+}
+class QueueComparator implements Comparator 
+{
+	public int compare(Object o1,Object o2)
+	{
+		String s1 = (String)o1;
+		String s2 = o2.toString();
+		return s2.compareTo(s1);
+	}
+}
