62b9f50bda0e991a6bcf3c3e467022f42dd70c21
shruthi <shruthi@thirumals-air>
2023-04-19 11:47:12 -0400

    apr19-commit

--- ExploringJava/firstpgm/UserPgm4.java
+++ ExploringJava/firstpgm/UserPgm4.java
@@ -9,6 +9,20 @@ public class UserPgm4
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter first name");
 		String fname = s.next();
+		System.out.println("enter last name");
+		String lname = s.next();
+		System.out.println("enter your grade");
+		int grade = s.nextInt();
+		System.out.println("enter your student id");
+		int sid = s.nextInt();
+		System.out.println("enter your login name");
+		String logName = s.next();
+		System.out.println("enter your GPA");
+		double gpa = s.nextDouble();
+		System.out.println("Name:"+fname+" "+lname);
+		System.out.println("grade:"+grade+"; "+"Student id:"+sid);
+		System.out.println("login name:"+logName);
+		System.out.println("GPA :"+gpa);
 	}
 
 }
