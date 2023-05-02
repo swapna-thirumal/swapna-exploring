e2740cfe25b321cab3cbcc92cb2300474606040e
shruthi <shruthi@thirumals-air>
2023-05-02 14:10:11 -0400

    may2-commit

--- ExploringJava/com/secondSetofProgram/SumAverageRunningIntUsingList_EnhancedforLoop.java
+++ ExploringJava/com/secondSetofProgram/SumAverageRunningIntUsingList_EnhancedforLoop.java
@@ -8,20 +8,21 @@ public class SumAverageRunningIntUsingList_EnhancedforLoop {
 	public static void main(String[] args) 
 	{
 		List <Integer> l = new ArrayList<Integer> ();
-		int lowerBound=1;
-		int upperBound=100;
+		int lowerBound=1111;
+		int upperBound=8899;
 			for(int i =lowerBound;i<=upperBound;i++)
 			{
 				l.add(i);
 			}
 			
-		int sum = 0;
+		int sum = 0,count=0;
 		for(int numbers:l)
 		{
 			sum+=numbers;
+			count++;
 		}
 		System.out.println("The sum of "+ lowerBound+ " to "+ upperBound+" is " +sum);
-		double average =(double)sum/upperBound;
+		double average =(double)sum/count;
 		System.out.println("The Average is "+ average);
 		
 
