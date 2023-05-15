a5c47bcf79d6fa42d68925ff7ab7b90012c1ee19
shruthi <shruthi@thirumals-air>
2023-05-14 21:21:14 -0400

    may14-commit

---
+++ ExploringJava/practice/SeparatingDigits.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.Scanner;
+
+public class SeparatingDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter your number");
+		int num = sc.nextInt();
+		int number = num;
+		sc.close();
+		 int sum=0;
+		while(num>0)
+		{
+			sum = sum+((num%10)*(num%10)*(num%10));
+			num=num/10;
+		}
+		if(number==sum)
+			System.out.println( number +" is Armstrong number");
+		else
+			System.out.println( number +" is  not Armstrong number");
+			
+	}
+
+}
